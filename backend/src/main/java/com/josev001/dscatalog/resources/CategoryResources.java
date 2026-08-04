b94ddb81c31c3b5e237ffd1ce8e33e983b15baea
José victor <Leaphaz@gmail.com>
2026-08-04 20:57:02 -0300

    chore: add Category resource

---
+++ backend/src/main/java/com/josev001/dscatalog/resources/CategoryResources.java
@@ -0,0 +1,26 @@
+package com.josev001.dscatalog.resources;
+
+import com.josev001.dscatalog.entities.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResources {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L,"Books"));
+        list.add(new Category(2L,"Electronics"));
+        return ResponseEntity.ok().body(list);//deixa eu responder uma resposta 200(ok)
+    }
+
+}
